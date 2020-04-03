f0ffca6e8144a6bedd344a5de3a83d69aaa6bcc3
Азиз Исматов <yourGitHub@example.co>
2020-04-03 23:00:56 +0500

    Создал  пользовательский конструкт и 2 метода

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyfirstClass
+{
+    class Program
+    {
+        static void Main()
+        {
+          double a,b;
+          a=Convert.ToInt32(Console.ReadLine());
+          b=Convert.ToInt32(Console.ReadLine());
+          Rectangle Aziz =new Rectangle(a,b);
+          Aziz.AreaCalculator();
+          Aziz.PerimetrCalculator();
+          
+        }
+    }
+    class Rectangle
+    {
+      public double side1,side2;
+      public double  area { get; set; }
+      public double perimetr { get; set; }
+      public Rectangle(double side1,double side2)
+      {
+        this.side1=side1;
+        this.side2=side2;
+      }
+      public void AreaCalculator()
+      {
+        
+      }
+       public void PerimetrCalculator()
+      {
+       
+      }
+      
+    }
+}
+
+ 
